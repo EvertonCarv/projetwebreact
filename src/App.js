f738fa8f9f88e77a9d5e77b43c50fd75995ef832
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2022-10-30 19:11:12 -0300

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
+
+import Main from './pages/Main/index'
+import Rodape from './pages/Main/Rodape'
+import Cabecalho from "./pages/Main/Cabecalho"
+import Contato from './pages/Contato/index'
+import Produto from "./pages/SobreEmpresa/Produto";
+import Diferenciais from "./pages/Diferenciais/Diferenciais"
+import Usuario from "./pages/Usuario/Usuario";
+
+
+class App extends Component {
+  render() {
+    return(
+      <Router>
+      <div class="App">
+         <Cabecalho />
+
+         <li> <Link to='/'>Home</Link></li>
+         <li> <Link to='/contato'>Contato</Link></li>
+         <li> <Link to='/produto'>Produto</Link></li>
+         <li> <Link to='/diferenciais'>Diferenciais</Link></li>
+         <li> <Link to='/usuario'>Usuario</Link></li>
+          <Routes>
+        <Route exact path='/' element={<Main/>}/>
+        <Route path='/contato' element={<Contato/>}/>
+        <Route path='/produto' element={<Produto/>}/>
+        <Route path='/diferenciais' element={<Diferenciais/>}/>
+        <Route path='/usuario' element={<Usuario/>}/>
+      </Routes>
+          <Rodape />         
+      </div>
+     
+      </Router>
+    );
+  }
 }
 
 export default App;
